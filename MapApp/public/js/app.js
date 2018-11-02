07fe7b4367f643f80336a0ada61ef4870c81e30a
Joshua Liang <joshcliang@yahoo.com>
2018-11-02 11:00:11 -0700

    Update MapApp/public/js/app.js

--- MapApp/public/js/app.js
+++ MapApp/public/js/app.js
@@ -1,19 +1,24 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services. Note the use of ngRoute.
 var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute'])
 
-    // Configures Angular routing -- showing the relevant view and controller when needed.
-    .config(function($routeProvider){
+  // Configures Angular routing -- showing the relevant view and controller when needed.
+  .config(function($routeProvider) {
 
-        // Add Bathroom Control Panel
-        $routeProvider.when('/add', {
-            controller: 'addCtrl',
-            templateUrl: 'partials/addForm.html',
+    // Add Bathroom Control Panel
+    $routeProvider.when('/add', {
+      controller: 'addCtrl',
+      templateUrl: 'partials/addForm.html',
 
-            // Find Bathrooms Control Panel
-        }).when('/find', {
-            controller: 'queryCtrl',
-            templateUrl: 'partials/queryForm.html',
-
-            // All else forward to the Find Bathroom Control Panel
-        }).otherwise({redirectTo:'/add'})
-    });
+      // Find Bathrooms Control Panel
+    }).when('/find', {
+      controller: 'queryCtrl',
+      templateUrl: 'partials/queryForm.html',
+      // Rate Bathroom Control Panel
+    }).when('/rate', {
+      controller: 'rateCtrl',
+      templateUrl: 'partials/rateForm.html',
+      // All else forward to the Find Bathroom Control Panel
+    }).otherwise({
+      redirectTo: '/add'
+    })
+  });
