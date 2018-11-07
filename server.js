095939470972a40cc161be098973a385c408c1c0
Joshua Liang <joshcliang@yahoo.com>
2018-11-07 03:04:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // -----------------------------------------------------
 var express         = require('express');
 var mongoose        = require('mongoose');
-var port            = process.env.PORT || 3000;
+
 var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
@@ -13,13 +13,14 @@ var app             = express();
 // -----------------------------------------------------
 // Sets the connection to MongoDB
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/MeanMapApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MeanMapApp');
 // Logging and Parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
 app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
 app.use(morgan('dev'));                                         // log with Morgan
 app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));               
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
@@ -30,5 +31,6 @@ require('./app/routes.js')(app);
 
 // Listen
 // -------------------------------------------------------
+var port            = process.env.PORT || 3000;
 app.listen(port);
 console.log('App listening on port ' + port);
