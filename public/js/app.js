7ce71708be4785afbd48f2ce2e725150fb12a35a
Joshua Liang <joshcliang@yahoo.com>
2018-11-17 12:02:42 -0800

    add rateCtrl controller for rateForm

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,25 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services. Note the use of ngRoute.
-var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute'])
+var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'rateCtrl', 'geolocation', 'gservice', 'ngRoute'])
 
   // Configures Angular routing -- showing the relevant view and controller when needed.
   .config(function($routeProvider) {
 
     // Add Bathroom Control Panel
     $routeProvider.when('/add', {
-      controller: 'addCtrl',
-      templateUrl: 'partials/addForm.html',
+        controller: 'addCtrl',
+        templateUrl: 'partials/addForm.html',
 
-      // Find Bathrooms Control Panel
-    }).when('/find', {
-      controller: 'queryCtrl',
-      templateUrl: 'partials/queryForm.html',
-      // Rate Bathroom Control Panel
-    }).when('/rate', {
-      controller: 'rateCtrl',
-      templateUrl: 'partials/rateForm.html',
-      // All else forward to the Find Bathroom Control Panel
-    }).otherwise({
-      redirectTo: '/add'
-    })
+        // Find Bathrooms Control Panel
+      }).when('/find', {
+        controller: 'queryCtrl',
+        templateUrl: 'partials/queryForm.html',
+        // Rate Bathroom Control Panel
+      }).when('/rate', {
+        controller: 'rateCtrl',
+        templateUrl: 'partials/rateForm.html',
+        // All else forward to the Find Bathroom Control Panel
+      })
+      .otherwise({
+        redirectTo: '/add'
+      })
   });
