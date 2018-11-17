17b02195b4cdb656a0c03f8d2d07fff40b082cef
Joshua Liang <joshcliang@yahoo.com>
2018-11-17 12:03:12 -0800

    delete listCtrl

--- public/js/listCtrl.js
+++
@@ -1,3 +0,0 @@
-// Creates the listCtrl Module and Controller. Note that it depends on 'geolocation' and 'gservice' modules.
-var listCtrl = angular.module('queryCtrl', ['geolocation', 'gservice']);
-listCtrl.controller('queryCtrl', function($scope, $log, $http, $rootScope, geolocation, gservice) {
