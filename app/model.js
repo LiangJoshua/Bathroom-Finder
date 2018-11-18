54203d07b9bae554378a06db803e34882624d421
Joshua Liang <joshcliang@yahoo.com>
2018-11-17 17:06:08 -0800

    fix ratings

--- app/model.js
+++ app/model.js
@@ -4,20 +4,22 @@ var Schema      = mongoose.Schema;
 
 // Creates a User Schema. This will be the basis of how user data is stored in the db
 var UserSchema = new Schema({
-    username: {type: String, required: true},
+    bathroomName: {type: String, required: true},
     gender: {type: String, required: true},
     rating: {type: Number, required: true},
+    avgRating: {type: Number, required: true},
+    ratingCount: {type: Number, required: true},
     location: {type: [Number], required: true}, // [Long, Lat]
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
 });
 
 // Sets the created_at parameter equal to the current time
 UserSchema.pre('save', function(next){
     now = new Date();
-    this.updated_at = now;
-    if(!this.created_at) {
-        this.created_at = now
+    this.updatedAt = now;
+    if(!this.createdAt) {
+        this.createdAt = now
     }
     next();
 });
@@ -25,5 +27,5 @@ UserSchema.pre('save', function(next){
 // Indexes this schema in 2dsphere format (critical for running proximity searches)
 UserSchema.index({location: '2dsphere'});
 
-// Exports the UserSchema for use elsewhere. Sets the MongoDB collection to be used as: "MeanApp"
+// Exports the UserSchema for use elsewhere. Sets the MongoDB collection to be used as: "bathrom"
 module.exports = mongoose.model('bathroom', UserSchema);
