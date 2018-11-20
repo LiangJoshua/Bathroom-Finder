ab89525dc390ea5f6ca53fc473008ef318ec43a4
Joshua Liang <joshcliang@yahoo.com>
2018-11-19 21:37:12 -0800

    Add jest test for app.post

---
+++ app/post.test.js
@@ -0,0 +1,27 @@
+// White-box test case using jest and supertest
+// Tests if the our route app.post will add bathrooms to MongoDB Database
+const request = require('supertest');
+const express = require('express');
+var app = express();
+var users = [{
+  bathroomName: "Jack in the Box",
+  gender: "Male",
+  rating: 18,
+  avgRating: 3.5,
+  ratingCount: 5,
+  location: [-121.88840310870665, 37.35725874484297],
+  __v: 0,
+  updatedAt: "2018-11-18T01:48:23.585Z",
+  createdAt: "2018-11-18T00:43:00.666Z"
+}];
+app.post('/users', function(req, res) {
+  res.json(users);
+});
+describe('Test the test path with POST', function() {
+
+  it('It should work', function() {
+    request(app)
+      .post('/users')
+      .expect(200)
+  });
+});
